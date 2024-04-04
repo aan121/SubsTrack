09408cb924fbda96af185db7f8ca31172a598cda
Abdulwahab <aan121@hotmail.com>
2024-04-04 12:59:33 -0400

    .

--- app/substrack/lib/notification/notification_model.dart
+++
@@ -1,7 +0,0 @@
-class NotificationModel {
-  late String notifyDay,notifyMonth,notifyMessage;
-  int expireDay;
-
-  NotificationModel(this.notifyDay,this.notifyMonth, this.notifyMessage,this.expireDay,);
-
-}
