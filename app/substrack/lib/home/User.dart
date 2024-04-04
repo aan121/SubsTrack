64aa8182daf4eb8352863336b457897640584ebc
Abdulwahab <aan121@hotmail.com>
2024-04-04 13:00:11 -0400

    .

---
+++ app/substrack/lib/home/User.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+class User {
+ late String name,date,price;
+ User(this.name, this.date, this.price);
+}
---
+++ app/substrack/lib/notification/notification_model.dart
@@ -0,0 +1,7 @@
+class NotificationModel {
+  late String notifyDay,notifyMonth,notifyMessage;
+  int expireDay;
+
+  NotificationModel(this.notifyDay,this.notifyMonth, this.notifyMessage,this.expireDay,);
+
+}
