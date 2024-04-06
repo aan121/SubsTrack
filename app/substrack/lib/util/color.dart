614a070b8a3cff94d9f95ca1aed7b94a00fc8f78
Abdulwahab <aan121@hotmail.com>
2024-04-06 18:04:54 -0400

    .

--- app/substrack/lib/util/color.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class ThemeColors {
-  static const Color background = Color(0xFF030D30);
-  static const Color backgroundGray = Color(0xFF999999);
-  static const Color tabColor = Color(0xFF68DCF5);
-
-}
